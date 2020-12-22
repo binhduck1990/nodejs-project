ab703af42f5cca3acaf789d3ca8d8b9f3cb7f189
binh <binhduck1990@gmail.com>
2020-12-22 06:48:48 -0800

    comment

--- middleware/AuthToken.js
+++ middleware/AuthToken.js
@@ -15,6 +15,7 @@ checkToken = async (req, res, next) => {
         try {
             var decodedToken = await jwt.verify(token, process.env.SECRET_KEY)
         } catch (decode_token_error) {
+            // Xử lý lỗi khi giải mã token
             if(decode_token_error.message === 'jwt expired'){
                 try {
                     if(!refreshToken){
@@ -22,43 +23,50 @@ checkToken = async (req, res, next) => {
                             message: 'token expired'
                         })
                     }
-    
+                    
                     try {
                         var decodedRefreshToken = await jwt.verify(refreshToken, process.env.SECRET_KEY)
                     } catch (decode_refresh_token_error) {
+                        // Xử lý lỗi khi giải mã refresh_token
                         return res.status(401).json({
                             message: decode_refresh_token_error.message
                         })
                     }
                     
+                    // Tìm user hợp lệ theo id và refresh_token
                     const user = await userModel.findOne({_id: decodedRefreshToken.id, refresh_token: refreshToken})
                     if(!user){
                         return res.status(401).json({
                             message: 'refresh_token not found'
                         })
                     }
-         
+                    
                     try {
                         var generatedToken = await userService.generateToken(user)
                     } catch (generate_token_error) {
+                        // Xử lý lỗi khi tạo mới token, refresh_token
                         return res.status(401).json({
                             message: generate_token_error.message
                         })
                     }
+                    
                     res.token = generatedToken.token
                     res.refresh_token = generatedToken.refreshToken
                     return next()
                 } catch (error) {
+                    // Xử lý lỗi server
                     return res.status(404).json({
                         message: error.message
                     })
                 }
             }
+            // Nếu giải mã token lỗi các TH khác thì cảnh báo
             return res.status(401).json({
                 message: decode_token_error.message
             })
         }
 
+        // Nếu token hợp lệ thì kiểm tra tiếp các thông tin khác trong token
         if(decodedToken && decodedToken.type !== 'token'){
             return res.status(401).json({
                 message: 'invalid token'
@@ -78,8 +86,10 @@ checkToken = async (req, res, next) => {
                 message: 'token expired'
             })
         }
+        // token hợp lệ
         next() 
     } catch (error) {
+        // Xử lý lỗi server
         return res.status(404).json({
             message: error.message
         })
