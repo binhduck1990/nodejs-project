bdd018dbab3c219a0b44fb4fde67b91d3b94c076
binh <an duong>
2020-12-24 14:33:13 +0700

    check quyen

--- middleware/Role.js
+++ middleware/Role.js
@@ -1,12 +1,12 @@
 canManageUser = (req, res, next) => {
     const user = res.user
     const id = req.params.id
-    if(user.role !== 'admin' || !user._id.equals(id)){
-        return res.status(401).json({
-            message: 'permission denied'
-        })
+    if(user.role === 'admin' || user._id.equals(id)){
+        return next()
     }
-    next()
+    return res.status(401).json({
+        message: 'permission denied'
+    })  
 }
 
 module.exports = {
